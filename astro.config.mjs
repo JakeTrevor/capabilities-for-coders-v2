1b84ce096a0a26d4c9af6257e3bf715a86be2433
Jake Trevor <jacob.trevor234@gmail.com>
2024-08-27 15:09:58 +0100

    added files/code for v2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,70 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
+import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  markdown: {
+    rehypePlugins: [[rehypeMermaid, { strategy: "pre-mermaid", dark: true }]],
+  },
+  integrations: [
+    starlight({
+      head: [
+        {
+          tag: "script",
+          attrs: { type: "module" },
+          content:
+            "import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs'; mermaid.initialize({ startOnLoad: true });",
+        },
+      ],
+      title: "Capabilities for Coders",
+      logo: { src: "./src/assets/cherry_bakewell.png" },
+      favicon: "/favicon-32x32.png",
+      social: { github: "https://github.com/withastro/starlight" },
+      plugins: [starlightLinksValidator()],
+      sidebar: [
+        {
+          label: "Cheri",
+          items: [
+            "cheri/what-is-cheri",
+            "cheri/capabilities",
+            {
+              collapsed: true,
+              label: "Compiling for CHERI",
+              items: ["cheri/compiling/purecap", "cheri/compiling/hybrid"],
+            },
+            "cheri/running",
+            "cheri/header",
+            "cheri/purecap-and-hybrid",
+            "cheri/printf",
+            "cheri/purposes_of_sealed_capabilities",
+            "cheri/how_to_seal",
+            "cheri/setbounds",
+            "cheri/tagcheck",
+          ],
+        },
+        {
+          label: "Morello",
+          items: [
+            "morello/get-a-board",
+            "morello/how_to_tell_morello",
+            "morello/teardown",
+            "morello/python",
+            "morello/compartmentalisation",
+          ],
+        },
+        {
+          label: "CheriBSD",
+          items: [
+            "cheri-bsd/compile_on_freebsd",
+            "cheri-bsd/configure_networking",
+            "cheri-bsd/hardware_performance_counters",
+            "cheri-bsd/ldpreload",
+            "cheri-bsd/shared_library_path",
+          ],
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/cheri/what-is-cheri.md
@@ -0,0 +1,17 @@
+---
+title: What is CHERI?
+description: TODO
+---
+
+:::danger[TODO]
+this
+:::
+
+CHERI stands for 'Capability Hardware Enhanced RISC Instructions': this is a bolt-on to a standard RISC instruction set architecture to provide support for capabilities in the processor architecture. The prototype [Arm Morello](https://www.arm.com/architecture/cpu/morello) processor supports capabilities natively in hardware.
+
+This page should include;
+
+- disambiguation of different implementations
+  - e.g. cheriot, cheribsd, morello
+- cheribuild
+-
